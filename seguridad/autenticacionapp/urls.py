33244aa6fc5af59662602638056253894ebf577f
blandskron <bastianlandskronfreelancer@gmail.com>
2024-04-14 18:50:48 -0400

    update

---
+++ seguridad/autenticacionapp/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('mi-vista', views.MiVista.as_view(), name='mi_vista'),
+#     path('otra-vista/', views.OtraVista.as_view(), name='otra_vista'),
+#     path('acceso-denegado/', views.acceso_denegado, name='acceso_denegado'),
+#     path('accounts/login/', views.LoginView.as_view(), name='login'), 
+# ]
---
+++ seguridad/seguridad/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for seguridad project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView  
+from django.shortcuts import redirect  # Importa redirect
+from autenticacionapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', lambda request: redirect('login'), name='redirect_to_login'),
+    path('', lambda request: redirect('login'), name='redirect_to_login'),
+    path('mi-vista/', views.MiVista.as_view(), name='mi_vista'),
+    path('otra-vista/', views.OtraVista.as_view(), name='otra_vista'),
+    path('acceso-denegado/', views.acceso_denegado, name='acceso_denegado'),
+]
+
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth.views import LoginView
+# from autenticacionapp import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # Ruta para la página de inicio de sesión
+#     path('login/', LoginView.as_view(), name='login'),
+#     # Incluye las URLs de autenticación de Django
+#     path('', include('django.contrib.auth.urls')),
+#     # Otras URLs personalizadas aquí...
+#     path('mi-vista/', views.MiVista.as_view(), name='mi_vista'),
+#     path('otra-vista/', views.OtraVista.as_view(), name='otra_vista'),
+#     path('acceso-denegado/', views.acceso_denegado, name='acceso_denegado'),
+# ]
+
